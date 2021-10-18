73d2cad4fe203afbfe01ac79bb64b64a3c7a60b5
Raphael Zöllner <raphael.zoellner@outlook.com>
2021-10-18 22:57:59 +0200

    Add initial mock grpc backend

--- cmd/backend/main.go
+++ cmd/backend/main.go
@@ -1,9 +1,29 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/TrashRaiders/garbage-truck/pkg/protos"
+	"github.com/TrashRaiders/garbage-truck/pkg/shop"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+)
 
 func main() {
 	logger := logrus.StandardLogger()
 
-	logger.Infof("backend started")
+	port := 8080
+	lis, err := net.Listen("tcp", fmt.Sprintf("localhost:%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	var opts []grpc.ServerOption
+	grpcServer := grpc.NewServer(opts...)
+	protos.RegisterShopServiceServer(grpcServer, shop.NewService())
+
+	logger.Infof("backend listening on port: %v", port)
+
+	grpcServer.Serve(lis)
 }
